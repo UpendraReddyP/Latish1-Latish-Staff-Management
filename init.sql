99f63dbf6f456c7536e632e90c218b2e5669e3bd
UpendraReddyP <upendrareddyp7@gmail.com>
2025-07-15 19:42:55 +0530

    updated

--- init.sql
+++ init.sql
@@ -1,8 +1,11 @@
-CREATE TABLE IF NOT EXISTS staff (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  department VARCHAR(100),
-  email VARCHAR(100),
-  status VARCHAR(20) DEFAULT 'Active',
-  joining_date DATE
-);
+CREATE TABLE IF NOT EXISTS employees (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50) NOT NULL,
+                emp_id VARCHAR(7) UNIQUE NOT NULL,
+                email VARCHAR(64) UNIQUE NOT NULL,
+                role VARCHAR(50) NOT NULL,
+                joining_date DATE NOT NULL,
+                training VARCHAR(10) NOT NULL,
+                project_status VARCHAR(20) NOT NULL,
+                project_name VARCHAR(50)
+            )
